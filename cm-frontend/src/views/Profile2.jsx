f92b5969c206dfddc0104cecf622bbc9dfc29b26
Vivek Singhal <viveksinghal32454@gmail.com>
2022-09-16 21:03:49 +0530

    Profile Pages Committed

--- cm-frontend/src/views/Profile2.jsx
+++ cm-frontend/src/views/Profile2.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, renderMatches } from "react-router-dom";
 import { BsPencil } from "react-icons/bs";
-export default function Profile() {
+export default function Profile2() {
   return (
     <div className="row">
       <div className="col-3">
@@ -23,32 +23,44 @@ export default function Profile() {
           ></img>
         </div>
         <div style={{ marginLeft: "130px", marginTop: "-70px" }}>
-          <h5>Pradeep Maurya</h5>
-          <h6 style={{ color: "gray" }}>Student</h6>
-          <h6 style={{ color: "gray" }}>Branch B</h6>
+          <h5>Keshav</h5>
+          <h6 style={{ color: "gray" }}>Teacher</h6>
+          <h6 style={{ color: "gray" }}>Software Enginnering,abc</h6>
           <div style={{ color: "gray", marginTop: "30px" }}>
             <h6>Email</h6>
-            <h6>Level</h6>
-            <h6>Address</h6>
+            <h6>city</h6>
+            <h6>State</h6>
+            <h6>Country</h6>
             <h6>Phone</h6>
           </div>
-          <div style={{ marginTop: "-108px", marginLeft: "130px" }}>
-            <h6>Pradeep Maurya</h6>
-            <h6>class 6</h6>
-            <h6>Lucknow,up,India</h6>
+          <div style={{ marginTop: "-135px", marginLeft: "130px" }}>
+            <h6>Keshav@gmail.com</h6>
+            <h6>Lucknow</h6>
+            <h6>UP</h6>
+            <h6>India</h6>
             <h6>1234567890</h6>
           </div>
         </div>
         <div>
           <button
             style={{
-              marginLeft: "450px",
+              marginLeft: "350px",
               borderRadius: "25px",
               padding: "10px 25px",
               fontSize: "15px",
             }}
           >
             Verify
+          </button>
+          <button
+            style={{
+              marginLeft: "20px",
+              borderRadius: "25px",
+              padding: "10px 25px",
+              fontSize: "15px",
+            }}
+          >
+            Give FeedBack
           </button>
         </div>
         <div style={{ marginTop: "-250px", marginLeft: "450px" }}>
@@ -196,36 +208,103 @@ export default function Profile() {
           </div>
         </div>
         <div>
-          <div>
-            <img
-              src={require("../logo.svg")}
-              style={{
-                width: "70px",
-                height: "70px",
-                border: "2px solid black",
-                borderRadius: "50%",
-              }}
-              alt="Image"
-            ></img>
-          </div>
-          <div style={{ marginLeft: "130px", marginTop: "-70px" }}>
-            <h5>Reviewer Name(Student)</h5>
-            <div style={{ width: "600px" }}>
-              <h6 style={{ color: "gray" }}>
-                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-                am et sint. Velit officia consequat duis enim velit mollit.
-                Exercitation veniam consequat sunt nostrud amet.
-              </h6>
+          <h5>FeedBack</h5>
+          <div style={{ marginTop: "20px" }}>
+            <div>
+              <img
+                src={require("../logo.svg")}
+                style={{
+                  width: "70px",
+                  height: "70px",
+                  border: "2px solid black",
+                  borderRadius: "50%",
+                }}
+                alt="Image"
+              ></img>
             </div>
-            <hr
-              style={{
-                width: "75%",
-                height: "2px",
-                borderWidth: "5",
-                color: "black",
-                backgroundColor: "black",
-              }}
-            />
+            <div style={{ marginLeft: "130px", marginTop: "-70px" }}>
+              <h5>Reviewer Name(Student)</h5>
+              <div style={{ width: "600px" }}>
+                <h6 style={{ color: "gray" }}>
+                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                  am et sint. Velit officia consequat duis enim velit mollit.
+                  Exercitation veniam consequat sunt nostrud amet.
+                </h6>
+              </div>
+              <hr
+                style={{
+                  width: "75%",
+                  height: "2px",
+                  borderWidth: "5",
+                  color: "black",
+                  backgroundColor: "black",
+                }}
+              />
+            </div>
+
+            <div>
+              <img
+                src={require("../logo.svg")}
+                style={{
+                  width: "70px",
+                  height: "70px",
+                  border: "2px solid black",
+                  borderRadius: "50%",
+                }}
+                alt="Image"
+              ></img>
+            </div>
+            <div style={{ marginLeft: "130px", marginTop: "-70px" }}>
+              <h5>Reviewer Name(Student)</h5>
+              <div style={{ width: "600px" }}>
+                <h6 style={{ color: "gray" }}>
+                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                  am et sint. Velit officia consequat duis enim velit mollit.
+                  Exercitation veniam consequat sunt nostrud amet.
+                </h6>
+              </div>
+              <hr
+                style={{
+                  width: "75%",
+                  height: "2px",
+                  borderWidth: "5",
+                  color: "black",
+                  backgroundColor: "black",
+                }}
+              />
+            </div>
+
+            <div>
+              <img
+                src={require("../logo.svg")}
+                style={{
+                  width: "70px",
+                  height: "70px",
+                  border: "2px solid black",
+                  borderRadius: "50%",
+                }}
+                alt="Image"
+              ></img>
+            </div>
+            <div style={{ marginLeft: "130px", marginTop: "-70px" }}>
+              <h5>Reviewer Name(Student)</h5>
+              <div style={{ width: "600px" }}>
+                <h6 style={{ color: "gray" }}>
+                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                  am et sint. Velit officia consequat duis enim velit mollit.
+                  Exercitation veniam consequat sunt nostrud amet.
+                </h6>
+              </div>
+              <hr
+                style={{
+                  width: "75%",
+                  height: "2px",
+                  borderWidth: "5",
+                  color: "black",
+                  backgroundColor: "black",
+                }}
+              />
+            </div>
           </div>
         </div>
       </div>
